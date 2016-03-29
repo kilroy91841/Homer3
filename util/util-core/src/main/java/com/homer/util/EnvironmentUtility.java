b6eb7e534242aa9d9723aeeef09d6ebee65252f3
Ari Golub <arigolub@gmail.com>
2016-03-29 07:49:59 -0400

    no more clean

--- util/util-core/src/main/java/com/homer/util/EnvironmentUtility.java
+++ util/util-core/src/main/java/com/homer/util/EnvironmentUtility.java
@@ -33,8 +33,8 @@ public class EnvironmentUtility {
             setDatabasePassword(System.getProperty(BOXFUSE_DATABASE_PASSWORD));
             setInstanceIP(System.getProperty(BOXFUSE_INSTANCE_IP));
             setInstancePort(System.getProperty(BOXFUSE_PORTS_HTTP));
+            setShouldClean(false);
             //TODO don't just set this to true
-            setShouldClean(true);
             setShouldMigrate(true);
             setDatabaseSchema("homer");
 
