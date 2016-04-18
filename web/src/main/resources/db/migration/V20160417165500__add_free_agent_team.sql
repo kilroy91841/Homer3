50699266ec9c828c8c17d1265c3a05628bdc417c
Ari Golub <arigolub@gmail.com>
2016-04-17 21:14:02 -0400

    k

--- web/src/main/resources/db/migration/V20160417165500__add_free_agent_team.sql
+++ web/src/main/resources/db/migration/V20160417165500__add_free_agent_team.sql
@@ -1,5 +1,5 @@
 USE homer;
 
-INSERT INTO ref_mlb_team (id, mlbAbbreviation, name, abbreviation)
+INSERT IGNORE INTO ref_mlb_team (id, mlbAbbreviation, name, abbreviation)
 VALUES
 (1, "fa", "Free Agency", "FA");
